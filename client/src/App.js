18daac7a474d3179036ca785e3910d3e2606eb6e
Jesse-Tingle <jtingle0@gmail.com>
2019-09-08 22:24:14 -0400

    finished MVP and stretch goals

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,19 @@ const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+    console.log("movie", movie);
+    const foundMovie = savedList.find(el => el.title === movie.title)
+      if(!foundMovie) {
+        setSavedList( [...savedList, movie] );
+      } else {alert('Movie already exists')}}
+  
 
   return (
     <div>
       <SavedList list={savedList} />
       {/* <div>Replace this Div with your Routes</div> */}
       <Route path="/" exact component={MovieList} />
-      <Route path="/movies/:id" exact component={Movie} />
+      <Route path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={addToSavedList}/>} />
     
     </div>
   );
